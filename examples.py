e7ebaf645d9df723aaf2fca43dd78c7ad664b500
wwbyftx <38065169+wwbyftx@users.noreply.github.com>
2023-05-22 16:09:33 +0800

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,6 +1,6 @@
 import neware_ndax_reader
 
 
-file = r'D:\Program Files (x86)\cnbusiness-prod\user_12886\file\倍率充放电测试\15-5-1-11-倍率充放电测试-KCC0201-2-20221216230450.ndax'
+file = r'20221216230450.ndax'
 df,barcodes = neware_ndax_reader.read(file)
 print(df)
